815093d9d43f57bdb5615505f1d945c1c6489080
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-08-01 15:16:52 -0300

    create domain classes
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/java/br/com/erickrodrigs/myinvest/domain/Categoria.java
@@ -0,0 +1,62 @@
+package br.com.erickrodrigs.myinvest.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	
+	private String nome;
+	
+	private String descricao;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo, descricao, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(codigo, other.codigo) && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(nome, other.nome);
+	}
+}
